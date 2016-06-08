ea3ec7513c845f4e9dc24599a9666d05bae7ee66
ro-double-b <robb.marsee@gmail.com>
2016-06-08 15:00:24 -0700

    Make first 3 tests pass

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,20 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {0: 1};
+  var key = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[key] = value;
+    key++;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    return key;
   };
 
   return someInstance;
