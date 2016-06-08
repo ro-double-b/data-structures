d752e1453a8f6fc36bec3960ad502df65237aa28
ro-double-b <robb.marsee@gmail.com>
2016-06-08 15:05:51 -0700

    Complete functional pattern stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,21 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {0: 1};
   var key = 0;
+  var lastValue;
 
   // Implement the methods below
   someInstance.push = function(value) {
     storage[key] = value;
     key++;
+    lastValue = value;
   };
 
   someInstance.pop = function() {
+    if (key > 0) {
+      delete storage[key];
+      key--;      
+    }
+    return lastValue;
   };
 
   someInstance.size = function() {
