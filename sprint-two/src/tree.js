30ba4989309da33b1a6c22e89546b43c77a04d0d
ro-double-b <robb.marsee@gmail.com>
2016-06-09 10:01:38 -0700

    Complete tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -19,28 +19,17 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
-  var returnedValues = {};
-  var key = 0;
-  var that = this;
 
-  var walk = function() {
-    if (that.value === target) {
+  if (this.value === target) {
+    return true;
+  }
+  
+  for (var i = 0; i < this.childNum; i++) {
+    if (this.children[i].contains(target)) {
       return true;
     }
-    
-    for (i = 0; i < that.childNum; i++) {
-      returnedValues[key] = that.children[i].contains(target);
-      key++;
-    }
-    var result = false;
-    for (i = 0; i < key; i++) {
-      if (returnedValues[i] === true) {
-        result = true;
-      }
-    }  
-  };
-  walk();
-  console.log(returnedValues);
+  }
+  return false;     
 };
 
 
