4ce9b490bc9740f52a7e42ba4fecd1cc1b78076b
ro-double-b <robb.marsee@gmail.com>
2016-06-08 21:20:05 -0700

    Complete linkedList tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,29 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.head === null) {
+      list.head = Node(value);
+    }
+    if (list.tail !== null) {
+      list.head.next = Node(value);
+    }
+    list.tail = Node(value);
   };
 
   list.removeHead = function() {
+    var temp = list.head;
+    list.head = list.head.next;
+    return temp.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node = list.head) {
+    if (node.value === target) {
+      return true;
+    } 
+    if (node.next === null) {
+      return false;
+    }
+    return list.contains(target, node.next);
   };
 
   return list;
