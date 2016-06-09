55143afd4b051e009ef9a49d54f52bb44d91771e
ro-double-b <robb.marsee@gmail.com>
2016-06-08 22:52:48 -0700

    Complete two test for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,12 @@
 var Tree = function(value) {
   var newTree = {};
+  newTree.childNum = 0;
   newTree.value = value;
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = {};  // fix me
 
   return newTree;
 };
@@ -11,9 +14,33 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children[this.childNum] = Tree(value);
+  this.childNum++;
 };
 
 treeMethods.contains = function(target) {
+  var returnedValues = {};
+  var key = 0;
+  var that = this;
+
+  var walk = function() {
+    if (that.value === target) {
+      return true;
+    }
+    
+    for (i = 0; i < that.childNum; i++) {
+      returnedValues[key] = that.children[i].contains(target);
+      key++;
+    }
+    var result = false;
+    for (i = 0; i < key; i++) {
+      if (returnedValues[i] === true) {
+        result = true;
+      }
+    }  
+  };
+  walk();
+  console.log(returnedValues);
 };
 
 
