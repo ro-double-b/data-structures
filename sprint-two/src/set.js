0a9eddfd0d9cc5d7e66419686bf6f20a272c18c0
ro-double-b <robb.marsee@gmail.com>
2016-06-09 11:29:40 -0700

    Complete all tests for Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage.indexOf(item) === -1) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.indexOf(item) >= 0;
 };
 
 setPrototype.remove = function(item) {
+  var idx = this._storage.indexOf(item);
+  this._storage = this._storage.slice(0, idx).concat(this._storage.slice(idx + 1, this._storage.length));
 };
 
 /*
