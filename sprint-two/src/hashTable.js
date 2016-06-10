1164a0d09b168334986b2bbb68cc1059538d6873
ro-double-b <robb.marsee@gmail.com>
2016-06-09 19:30:33 -0700

    Complete all tests for hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,12 +9,38 @@ HashTable.prototype.insert = function(k, v) {
   if (this._storage.get(index) === undefined || k === this._storage.get(index)[0]) {
     var pair = [k, v];
     this._storage.set(index, pair);    
+  } else {
+
+    if (Array.isArray(this._storage.get(index)[0])) {
+      
+      var currentArray = this._storage.get(index);
+      currentArray.push([k, v]);
+      this._storage.set(index, currentArray);
+    } else {
+      
+      var result = [];
+      var temp = this._storage.get(index);
+      result.push(temp);
+      result.push([k, v]);
+      this._storage.set(index, result);     
+    }
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
+  debugger;
+  var result;
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage.get(index) !== undefined) {
+    if (Array.isArray(this._storage.get(index)[0])) {
+      var toCheck = this._storage.get(index);
+      toCheck.forEach(function(array) {
+        if (array[0] === k) {
+          result = array[1];
+        }
+      });
+      return result;
+    }
     return this._storage.get(index)[1];
   }
   
