8bb2878ce6799d7340472f95b4aa49988bd325c7
ro-double-b <robb.marsee@gmail.com>
2016-06-10 22:20:15 -0700

    Complete advanced tests for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,25 +1,80 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = [];
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  if (this._storage.indexOf(item) === -1) {
-    this._storage.push(item);
+  if (typeof(item) === 'function') {
+    if (this._storage[item.toString()] === undefined) {
+      this._storage[item.toString()] = [];
+    }
+    this._storage[item.toString()].push(item);
+  } else {
+    if (this._storage[JSON.stringify(item)] === undefined) {
+      this._storage[JSON.stringify(item)] = [];
+    }
+    this._storage[JSON.stringify(item)].push(item);
   }
 };
 
 setPrototype.contains = function(item) {
-  return this._storage.indexOf(item) >= 0;
+  var result = false;
+  if (this._storage[JSON.stringify(item)]) {
+    this._storage[JSON.stringify(item)].forEach(function(element) {
+      if (typeof(element) === typeof(item)) {
+        result = true;
+      }
+    });
+  } 
+  if (this._storage[item.toString()]) {
+    this._storage[item.toString()].forEach(function(element) {
+      if (typeof element === 'function') {
+        result = true;
+      }
+    });
+  }
+  return result;
 };
 
 setPrototype.remove = function(item) {
-  var idx = this._storage.indexOf(item);
-  this._storage = this._storage.slice(0, idx).concat(this._storage.slice(idx + 1, this._storage.length));
+  var typeArray;
+  if (typeof item === 'function') {
+    typeArray = this._storage[item.toString()];
+  } else {
+    typeArray = this._storage[JSON.stringify(item)];
+  }
+
+  if (typeArray) { 
+    if (typeArray.length === 1) {
+      if (typeof item === 'function') {
+        this._storage[item.toString()] = undefined;
+      } else {
+        this._storage[JSON.stringify(item)] = undefined;
+      }
+    } else {
+      var types = typeArray.map(function(element) {
+        return typeof element;
+      });
+
+      var itemType = typeof item;
+      var indexToRemove = types.indexOf(itemType);
+
+      typeArray = typeArray.slice(0, indexToRemove).concat.typeArray.slice(indexToRemove + 1, typeArray.length);
+
+      if (typeof item === 'function') {
+        this._storage[item.toString()] = typeArray;
+      } else {
+        this._storage[JSON.stringify(item)] = typeArray;
+      }
+    }
+  }
 };
+
+
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
